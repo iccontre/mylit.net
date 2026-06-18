fc8a033ed64437c1ebceae166649ab933e2c59be
iccontre <iccontre@ucsc.edu>
2026-06-18 22:16:24 +0000

    Add MYLIT UI assets

--- lit-app/constants/uiAssets.ts
+++ lit-app/constants/uiAssets.ts
@@ -2,12 +2,10 @@ export const uiAssets = {
   backgrounds: {
     default: require("../assets/ui/backgrounds/default-background.png"),
   },
-
   guides: {
     luna: require("../assets/ui/guides/luna-guide.png"),
     evie: require("../assets/ui/guides/evie-guide.png"),
   },
-
   references: {
     neutralHome: require("../assets/ui/references/neutral-home-reference.png"),
     progressHome: require("../assets/ui/references/progress-home-reference.png"),
